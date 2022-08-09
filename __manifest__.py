4488dfd2afc4a897b268743fc093474f6720632c
Justin Deng <jden@odoo.com>
2022-08-09 12:58:07 -0400

    updated manifest

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Timeslotting module',
+    'version': '1.0',
+    'license': 'OPL-1',
+    'author': 'Odoo Inc',
+    'category': '',
+    'summary': '',
+    'description': """
+    """,
+    'depends': ['base'],
+    'data': [],      
+}
--- models/project.py
+++ models/project.py
@@ -1,3 +1,11 @@
 from odoo import models, api
 
 class Project(models.Model):
+
+    _inherit = 'project.task'
+
+    @api.depends('planned_date_begin')
+    def _compute_planned_date_begin(self):
+        for task in self:
+            if task.x_studio_expect_time:
+                print(task.x_studio_expect_time, '\n \n \n \n')
