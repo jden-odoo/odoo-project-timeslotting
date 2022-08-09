a892196823513d0af9dde712bf851d77ee129af5
Lars Gartenberg <laga@odoo.com>
2022-08-09 14:54:38 -0400

    add event changes

--- models/project.py
+++ models/project.py
@@ -1,4 +1,4 @@
-from odoo import models, api
+from odoo import models, api, fields, _
 from datetime import timedelta
 
 class Project(models.Model):
@@ -13,4 +13,20 @@ class Project(models.Model):
                 minutes = (task.x_studio_expect_time - hours) * 60
                 task.planned_date_begin = task.planned_date_end - timedelta(hours=hours, minutes=minutes)
 
-        
+    is_blocking = fields.Boolean(string='Is Blocking', default=True)
+
+    @api.onchange('stage_id')
+    def _onchange_stage_id(self):
+        for task in self:
+            if task.stage_id.name == 'Blocking':
+                task.is_blocking = True
+
+    @api.onchange('stage_id')
+    def _onchange_stage_id(self):
+        print('_onchange_stage_id')
+        for task in self:
+            if task.stage_id.name == 'Done' and task.is_blocking:
+                # search the depend_on_ids for planned_end_date
+                print(task.depend_on_ids.ids[0])
+                #end_date = self.env['project.task'].search([('id', '=', task.depend_on_ids.id)])
+                #print('end date', end_date, '\n \n \n \n')
